5f11f43249e8d613beb9be2feef8633fad98576b
3Gv2apiUser <progmati12>
2015-06-06 17:39:21 +0200

    base database component

---
+++ sys/com/Database.php
@@ -0,0 +1,37 @@
+<?php
+/*************************************************************�
+ *  @package  Manhertz
+ *  @author   Tamas Manhertz
+ *  @version  V0.99.20150305
+ *************************************************************/
+
+namespace sys\com;
+
+// adodb library needs extra initializations but only once at the first time
+require_once(ROOT . "libs/adodb5/adodb.inc.php");
+
+/**
+ * Class Database
+ * @package sys\com
+ */
+
+
+class Database extends \sys\ServerComponent {
+
+	/***********************************************
+	 *   PROPERTIES
+	 ***********************************************/
+
+	/***********************************************
+	 *   CONSTRUCT
+	 ***********************************************/
+
+	/***********************************************
+	 *   PROTECTED METHODS
+	 ***********************************************/
+
+	/***********************************************
+	 *   PUBLIC METHODS
+	 ***********************************************/
+
+}
