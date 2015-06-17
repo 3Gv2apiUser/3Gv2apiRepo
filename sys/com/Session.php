948d44f185ff7581585642c448f516a4eabd76f6
3Gv2apiUser <progmati12>
2015-06-18 00:45:26 +0200

    session component

---
+++ sys/com/Session.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pylon
+ * Date: 2015-06-14
+ * Time: 12:32
+ */
+/*************************************************************�
+ *  @package  Manhertz
+ *  @author   Tamas Manhertz
+ *  @version  V0.99.20150614
+ *************************************************************/
+
+namespace sys\com;
+
+
+/**
+ * Class Session
+ * @package sys\com
+ */
+class Session extends \sys\ServerComponent {
+
+	/***********************************************
+	 *   PROPERTIES
+	 ***********************************************/
+	/**
+	 * @var string
+	 */
+	protected $session_id;
+	/**
+	 * @var string
+	 */
+	protected $session_name;
+	/***********************************************
+	 *   CONSTRUCT
+	 ***********************************************
+	public function __construct() {
+	}
+	 */
+	/***********************************************
+	 *   PROTECTED METHODS
+	 ***********************************************/
+	protected function onInitialize() {
+		if ($this->session_name) {
+			session_name($this->session_name);
+		}
+		if ($this->session_id) {
+			session_id($this->session_id);
+		}
+		session_start();
+	}
+	protected function onFinalize() {
+		session_write_close();
+		session_unset();
+	}
+	/***********************************************
+	 *   PUBLIC METHODS
+	 ***********************************************/
+	public function set_session_id( $session_id ) {
+		$this->session_id = $session_id;
+	}
+}
---
+++ sys/com/api/Session.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pylon
+ * Date: 2015-06-18
+ * Time: 0:19
+ */
+
+namespace sys\com\api;
+
+
+/**
+ * Class HTTP
+ * @package sys\api\com
+ */
+class Session extends \sys\com\Session {
+	/***********************************************
+	 *   PROPERTIES
+	 ***********************************************/
+	protected function onInitialize() {
+		/** @var \sys\com\api\HTTP $HTTP */
+		if ($HTTP = $this->getComponent("HTTP")) {
+			if ($session_id = $HTTP->getHeader("Authorization")) {
+				$this->set_session_id($session_id);
+			}
+		}
+		ini_set('session.use_cookies', '0');
+		parent::onInitialize();
+	}
+
+
+	/***********************************************
+	 *   PROTECTED METHODS
+	 ***********************************************/
+
+
+}
