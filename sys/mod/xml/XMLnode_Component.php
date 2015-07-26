dd44cf35e2e70a923c88d3899352061eeb7c2a6d
3Gv2apiUser <progmati12>
2015-07-26 20:01:51 +0200

    xmlnode

---
+++ sys/mod/xml/XMLnode_Component.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pylon
+ * Date: 2015.06.01.
+ * Time: 21:46
+ */
+
+namespace sys\mod\xml;
+
+
+/**
+ * Class XMLnode_Component
+ * @package sys\mod\xml
+ */
+class XMLnode_Component {
+
+} 
